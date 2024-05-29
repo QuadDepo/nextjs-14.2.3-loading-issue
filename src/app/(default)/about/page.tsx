443e533f4f2fc15a063b11fec30ace4aa9328e80
Ryan George <ryan@pathe-thuis.nl>
2024-05-29 16:51:08 +0200

    chore: remove unstable cache

--- src/app/(default)/about/page.tsx
+++ src/app/(default)/about/page.tsx
@@ -1,45 +1,27 @@
 import { Metadata } from "next";
 
-import { unstable_cache } from 'next/cache';
-
 export const runtime = "edge";
-
-async function getData() {
-  'use server';
-  try {
-    const data = await fetch("https://swapi.dev/api/people/1");
-    const json = await data.json();
-    
-    return json.name;
-  } catch (err) {
-    return null;
-  }
-}
-
-
-const getCachedName = unstable_cache(
-  async () => getData(),
-  ['get_name']
-);
 
 
 export async function generateMetadata(): Promise<Metadata> {
-  const name = await getCachedName();
+  const data = await fetch("https://swapi.dev/api/people/1");
+  const json = await data.json();
   await new Promise((resolve) => setTimeout(resolve, 3000));
   
   return {
-    title: name,
+    title: json.name,
   };
 }
 
 export default async function Page() {
-  const name = await getCachedName();
+  const data = await fetch("https://swapi.dev/api/people/1");
+  const json = await data.json();
 
   return (
     <div>
       <h1>About</h1>
       <p>This is the about page.</p>
-      {name && <p>{name}</p>}
+      {json.name && <p>{json.name}</p>}
     </div>
   );
 }
